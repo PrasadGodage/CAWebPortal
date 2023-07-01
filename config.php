640715c04f494798deac26c2c7fd44c08e51ae26
soulsoftShital <127095081+soulsoftShital@users.noreply.github.com>
2023-07-01 13:08:56 +0530

    commit cpanel file

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 
  //$con=mysqli_connect("localhost","root","","ca_db")or die(mysqli_error($con));
 
- $con=mysqli_connect("localhost","root","kadlag@2023","prashantkadlag_ca_db")or die(mysqli_error($con));
+ $con=mysqli_connect("localhost","prashantkadlag_root","kadlag@2023","prashantkadlag_ca_db")or die(mysqli_error($con));
 
  // For production
 
